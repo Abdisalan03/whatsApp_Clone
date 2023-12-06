1c67b447175d8110fd582a2233c182e5afb53658
unknown <Eng Abdirahman.abdirahmanabdisalan935@gmail.com>
2023-12-06 09:13:30 +0300

    whatsapp

---
+++ whatsapp_clone/whatsapp_clone/lib/models/chat.dart
@@ -0,0 +1,51 @@
+class Chat {
+  String name;
+  String message;
+  final String avatar;
+  String time;
+  Chat(
+      {required this.name,
+      required this.message,
+      required this.avatar,
+      required this.time});
+}
+
+List<Chat> chats = [
+  Chat(
+      message: "Send a photo",
+      name: "Eng Abdirahman(You) ",
+      avatar: "https://i.ibb.co/hWRG8Qj/how.jpg",
+      time: "10:24 PM"),
+  Chat(
+      message: "yow haye ?",
+      name: "my friend ",
+      avatar: "https://i.ibb.co/fXxZMm9/moha.jpg",
+      time: "1:24 PM"),
+  Chat(
+      message: "Hello, How are you?",
+      name: "Omar Ali",
+      avatar:
+          "https://i.ibb.co/c8Q3Ldr/Whats-App-Image-2023-09-07-at-17-28-39.jpg",
+      time: "11:30 PM"),
+  Chat(
+      message: "Eng logo design baan ubahnaa.",
+      name: "sunlight school",
+      avatar: "https://i.ibb.co/7grPdKc/png.jpg",
+      time: "12:24 AM"),
+  Chat(
+      message: "Hey, mom good night?",
+      name: "mom ",
+      avatar:
+          "https://i.ibb.co/D43fhjR/IMG-20220225-WA0007-removebg-preview.png",
+      time: "8:34 AM"),
+  Chat(
+      message: "I Missed you so much!",
+      name: "my sister...",
+      avatar: "https://i.ibb.co/mTdxXF2/missh-nawab-20210302-11-Copy.jpg",
+      time: "1:56 AM"),
+  Chat(
+      message: "Assignmets github saar?",
+      name: "Ustaad Ahmed",
+      avatar: "https://i.ibb.co/hXD1c1G/Abdi.jpg",
+      time: "8:05 AM"),
+];
